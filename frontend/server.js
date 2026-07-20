3ea96a358f8c2367705eca787734d13d9562fd3e
GulianaLulichacR <gulianalulichacr@gmail.com>
2026-07-20 00:18:33 -0500

    frontend deploy

--- frontend/server.js
+++ frontend/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
-const app = express();
+const fs = require('fs');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir archivos estáticos del build
+const API_URL = process.env.REACT_APP_API_URL || '';
+
+app.get('/', (req, res) => {
+  const html = fs.readFileSync(path.join(__dirname, 'build', 'index.html'), 'utf8');
+  const injected = html.replace(
+    '</head>',
+    `<script>window.API_URL="${API_URL}";</script></head>`
+  );
+  res.send(injected);
+});
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Servir index.html para todas las rutas (SPA)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
@@ -15,4 +25,3 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Frontend server running on port ${PORT}`);
 });
-
